2673819f550103cc6fedffd6c6887eed839c5453
Fabian Gunzinger <fa.gunzinger@gmail.com>
2021-06-28 08:19:16 +0100

    start using inpaths and outpath in main.
    Need to adapt makefile to work with aws paths, though.

--- Makefile
+++ Makefile
@@ -1,3 +1,16 @@
+# directories
+MDB_DATA := "s3://3di-data-mdb"
+MDB_RAW := $(MDB_DATA)/raw/mdb_000.csv
+MDB_CLEAN := $(MDB_DATA)/clean/mdb_000.parquet
+
 .PHONY: mdb
+# .DELETE_ON_ERROR: $(MDB_CLEAN)
+
 mdb:
-	@python preproc/make_data.py
+	echo "hello"
+
+# mdb: $(MDB_CLEAN)
+
+# $(MDB_CLEAN): $(MDB_RAW)
+	# @python preproc/make_data.py $^
+	echo $^
--- preproc/make_data.py
+++ preproc/make_data.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
 
+import argparse
+
 import aws
 
-def main():
-    fp = 's3://3di-data-mdb/raw/mdb_000.csv'
-    data = aws.s3read_csv(fp, sep='|')
+def parse_args(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('inpath', help='path to input file')
+    parser.add_argument('output', help='path to output file')
+    return parser.parse_args()
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    args = parse_args(args)
+    data = aws.s3read_csv(args.inpath, sep='|')
     print(data.head())
 
 
